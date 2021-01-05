404c94fdb0d6b081e00b8c2ad2fe9e3ee3dca072
lezcouu <lezcouu@gmail.com>
2021-01-05 14:12:27 -0300

    hola mundo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom"
 import Portfolio from "./components/Portfolio.jsx"
 
 function App() {
