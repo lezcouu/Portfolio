7fd5ed03b4ce90f67a6587e2ec3f606ea757be8e
lezcouu <lezcouu@gmail.com>
2021-01-05 13:59:37 -0300

    hola mundo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Portfolio from "./components/Portfolio.jsx"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Comienza App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route
+      exact path="/"
+      component={Portfolio}
+       />
+    </Router>
   );
 }
 
