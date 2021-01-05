bef5b91f5ef3b4e8881035509f010747e0bf62ff
lezcouu <lezcouu@gmail.com>
2021-01-05 14:42:50 -0300

    helloworld origin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom"
 import Portfolio from "./components/Portfolio.jsx"
 
 function App() {
   return (
-    <Router>
-    <Route 
-      exact path="/" 
-      component={Portfolio} 
-      />
-    </Router>
+    <div>
+    	<Portfolio />
+    </div>
   );
 }
 
